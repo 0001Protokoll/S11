c796d8f8fe6930328c1aa07be692a4ff1b49a531
ueberleda <38735213+ueberleda@users.noreply.github.com>
2018-07-28 12:26:25 +0200

    pythondatei etwas aufgeräumter

--- Daten für Robert (für Berechnung des spez. Drehwinkels.)/Tabellen erzeugen.py
+++ Daten für Robert (für Berechnung des spez. Drehwinkels.)/Tabellen erzeugen.py
@@ -33,52 +33,30 @@ data = [getData(i) for i in names]
 konz = np.genfromtxt(str('I_max   bei 589.37 nm vs konzentrationen.txt'), dtype=float)
 
 
-####Überschrift wird als Marker für die erhaltenen Infos verwendet ... 
-Überschrift=np.array([                                                          #n
-        'CO',                                                #0
-        'HCl',                                               #1
-        'HCl CSSD (SCF)',                                         #2
-        'HCl CSSD (MP2)',                                         #3
-        'HCl CSSD (MP3)',                                         #4
-        'CO CSSD (SCF)',                                             #5
-        'CO CSSD (MP2)',                                             #6
-        'CO CSSD (MP3)', 
-        ])
-###Latex erlaubt keine leerstellen. Hier werden die Speichernamen der Bilder festgelegt.
-Speicher=np.array([                 
-         'CO_b3lyp',                                                     #0
-        'HCl_b3lyp',                                                     #1
-        'HCl_CSSD(SCF)',                                                   #2
-        'HCl_CSSD(MP2)',                                                   #3
-        'HCl_CSSD(MP3)',                                                   #4
-        'CO_CSSD(SCF)',                                                    #5
-        'CO_CSSD(MP2)',                                                    #6
-        'CO_CSSD(MP3)',
-        ])
 
+#print (data)
 
-
-print (data)
-tmp=[]
+tmp=[]  #### leeren Array zum speichern erzeugen
 for n in range (13):
     tmp.append([])
 
 CO=''
 print (12) 
 
-
+###### txt dateien in Array sortiert speichern
 for n in range (12):
-    #print (data[n])
-    
-    for i in range (7):
+        for i in range (7):
         tmp[n].append(data[i][n][0])
     tmp[n].append(" bei "+str(data[i][n][1])+" nm")    
     
+    #### ein weiterer für die Konzentrationen
 for i in range (7):
     tmp[12].append(konz[i][0])
 tmp[12].append(" konzentrationen")   
 print (tmp)
 
+
+#### speichern in einer txt Datei
 f = open('workfile.txt', 'w')
 for item in tmp:
     f.write("%s\n" % item)
